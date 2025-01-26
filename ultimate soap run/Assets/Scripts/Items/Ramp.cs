12297c89898944f64ebcf0d6348d09e118f93213
mikenigro <mikenigro99@gmail.com>
2025-01-26 16:00:10 +0100

    add sound ramp e c

--- ultimate soap run/Assets/Scripts/Items/Ramp.cs
+++ ultimate soap run/Assets/Scripts/Items/Ramp.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Ramp : MonoBehaviour
 {
     [SerializeField] private float rampBoost = 5f;
+    public AudioSource jumpsound;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            jumpsound.Play();
             Vector3 boostDirection = gameObject.transform.forward * rampBoost;
             other.GetComponent<Rigidbody>().AddTorque(0, 0, 500);
             other.GetComponent<Rigidbody>().AddForce(boostDirection, ForceMode.Acceleration);
